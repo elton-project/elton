7f50410f2c05af138914130c8c7fefcc9fd03b27
nashio <dev@nashio-lab.info>
2015-06-04 13:43:28 +0900

    hideo

--- commands.go
+++ commands.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
 
 	"github.com/codegangsta/cli"
 
 	"./elton"
-	http "./http"
+	elhttp "./http"
 )
 
 var Commands = []cli.Command{
@@ -20,10 +25,9 @@ var commandClient = cli.Command{
 	Description: ``,
 	Action:      doClient,
 	Flags: []cli.Flag{
-		cli.StringFlag{
-			Name:  "file, f",
-			Value: "proxy_config.tml",
-			Usage: "config file",
+		cli.BoolFlag{
+			Name:  "R, r",
+			Usage: "recursive",
 		},
 	},
 }
@@ -47,20 +51,89 @@ var commandServer = cli.Command{
 }
 
 func doClient(c *cli.Context) {
+	log.SetPrefix("[elton client] ")
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	r := c.Bool("R")
+	args := c.Args()
+
+	if r {
+		if err := filepath.Walk(args[0], func(p string, info os.FileInfo, err error) error {
+			if info.IsDir() {
+				return nil
+			}
+
+			return uploadFile(p, path.Join(args[1], p))
+		}); err != nil {
+			log.Println(err)
+		}
+		return
+	}
+
+	info, _ := os.Stat(args[0])
+	if info.IsDir() {
+		log.Printf("expected -R option")
+		return
+	}
+
+	if err := uploadFile(args[0], args[1]); err != nil {
+		log.Println(err)
+	}
 }
 
 func doServer(c *cli.Context) {
 	log.SetPrefix("[elton server] ")
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	conf, err := elton.Load(c.String("file"))
+
 	backup := c.Bool("backup")
+	conf, err := elton.Load(c.String("file"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	server, err := http.NewEltonServer(conf, backup)
+	server, err := elhttp.NewEltonServer(conf, backup)
 	if err != nil {
 		log.Fatal(err)
 	}
 	server.Serve()
+}
+
+func uploadFile(name string, url string) error {
+	// file, err := os.Open(name)
+	// if err != nil {
+	// 	return err
+	// }
+	// defer file.Close()
+
+	// body := new(bytes.Buffer)
+	// writer := multipart.NewWriter(body)
+
+	// part, err := writer.CreateFormFile("file", name)
+	// if err != nil {
+	// 	writer.Close()
+	// 	return err
+	// }
+
+	// if _, err = io.Copy(part, file); err != nil {
+	// 	writer.Close()
+	// 	return err
+	// }
+	// writer.Close()
+
+	// args := strings.Split(url, "/")
+	// req, _ := http.NewRequest("PUT", "http://"+path.Join(args[0], "elton", strings.Join(args[1:], "/")), body)
+	// req.Header.Add("Content-Type", writer.FormDataContentType())
+
+	// client := new(http.Client)
+	// res, err := client.Do(req)
+	// if err != nil {
+	// 	return err
+	// } else if res.StatusCode != http.StatusOK {
+	// 	return fmt.Errorf("http status: %v", res.StatusCode)
+	// }
+	// defer res.Body.Close()
+
+	// return nil
+	args := strings.Split(url, "/")
+	return fmt.Errorf("%s: %s", name, "http://"+path.Join(args[0], "elton", strings.Join(args[1:], "/")))
 }
