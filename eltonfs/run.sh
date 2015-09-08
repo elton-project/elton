1ad9c6a75e65a394bcddf2050d0300f4aeda5178
nashio <dev@nashio-lab.info>
2015-09-08 11:52:31 +0900

    hideo

--- eltonfs/run.sh
+++ eltonfs/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 /usr/sbin/munin-node > /dev/null 2>&1 &
-/elton/bin/eltonfs elton1:12345 --upperdir=upper --lowerdir=lower --host=elton_eltonfs_1 mountpoint
+/eltonfs/bin/eltonfs elton1:12345 --upperdir=upper --lowerdir=lower --host=elton_eltonfs_1 mountpoint
