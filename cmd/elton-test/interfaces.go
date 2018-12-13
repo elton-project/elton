0f6b5ba0fd1b3dc2f9154b644fe79cb9186811cb
yuuki <yuuki0xff@gmail.com>
2018-12-13 14:33:58 +0900

    add インターフェースの詳細な説明

--- cmd/elton-test/interfaces.go
+++ cmd/elton-test/interfaces.go
@@ -45,18 +45,24 @@ type Service interface {
 	ServiceType() ServiceType
 
 	// Serve()は、サービスを提供するメソッドである。
+	// コンテキストが終了するまで、受信したリクエストは全て処理するべきである。
+	// コンテキストが終了前に、能動的にreturnすることも可能だが、やるべきではない。
 	Serve(info *ServerInfo) error
 
 	// Created()は、サーバ起動直前の初期化処理をする。
 	// このメソッドは、Serve()が実行される前に呼び出される。
+	// 既にlistenしているのでアドレスやポート番号は決まっているが、いかなる接続もacceptされない。
 	Created(info *ServerInfo) error
 	// Running()は、サーバの起動直後に行なうべき処理をする。
-	// Running()とServe()は、それぞれgoroutineで並行処理される。
+	// Running()とServe()は並行処理されるが、同時に実行されることは保証しない。
+	// タイミング次第では、まだServe()が実行開始されていない状態でRunning()が実行される可能性がある。
 	Running(info *ServerInfo) error
 	// Prestop()は、サーバの終了直前に行うべき処理をする。
-	// Prestop()とServe()は、それぞれgoroutineで並行処理される。
+	// このメソッドの実行終了後にServe()が終了する。
+	// 通常はPrestop()とServe()は並行処理される。しかし、Serve()が先に終了する可能性もあることに注意。
 	Prestop(info *ServerInfo) error
 	// Stopped()は、サーバの終了後に行うべき処理をする。
 	// Serve()の実行終了後に呼び出される。
+	// まだlistenしているが、いかなる接続もacceptされない。
 	Stopped(info *ServerInfo) error
 }
