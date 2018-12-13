fc4eee34e4685bfb5f3f33fada05964c9f91cdbe
yuuki <yuuki0xff@gmail.com>
2018-12-13 16:02:23 +0900

    fix example service

--- cmd/elton-test/example.go
+++ cmd/elton-test/example.go
@@ -38,32 +38,44 @@ type ExampleDelivererService struct {
 	server ExampleDelivererServer
 }
 
+func (s *ExampleDelivererService) String() string {
+	return "<Service: " + s.Name() + ">"
+}
 func (s *ExampleDelivererService) Name() string {
-	return "<Service: Example/Deliverer>"
+	return "Example/Deliverer"
 }
-func (s *ExampleDelivererService) SetAddr(addr net.Addr) {
-	s.addr = addr
+func (s *ExampleDelivererService) SubsystemType() SubsystemType {
+	return UnknownSubsystemType
 }
-func (s *ExampleDelivererService) Register(ctx context.Context) error {
+func (s *ExampleDelivererService) ServiceType() ServiceType {
+	return UnknownServiceType
+}
+func (s *ExampleDelivererService) Serve(info *ServerInfo) error {
+	return WithGrpcServer(info.Ctx, func(srv *grpc.Server) error {
+		proto2.RegisterEventDelivererServer(srv, &s.server)
+		return srv.Serve(info.Listener)
+	})
+}
+func (s *ExampleDelivererService) Created(info *ServerInfo) error {
+	return nil
+}
+func (s *ExampleDelivererService) Running(info *ServerInfo) error {
 	return WithGrpcConn(s.addr, func(conn *grpc.ClientConn) error {
 		c := proto2.NewEventManagerClient(conn)
-		c.ListenStatusChanges(ctx, &proto2.EventDelivererInfo{
+		c.ListenStatusChanges(info.Ctx, &proto2.EventDelivererInfo{
 			ServerInfo: proto2.NewServerInfo(s.addr),
 		})
 		return nil
 	})
 }
-func (s *ExampleDelivererService) Unregister(ctx context.Context) error {
+func (s *ExampleDelivererService) Prestop(info *ServerInfo) error {
 	// todo
 	return WithGrpcConn(s.addr, func(conn *grpc.ClientConn) error {
 		return nil
 	})
 }
-func (s *ExampleDelivererService) Serve(ctx context.Context, listener net.Listener) error {
-	return WithGrpcServer(ctx, func(srv *grpc.Server) error {
-		proto2.RegisterEventDelivererServer(srv, &s.server)
-		return srv.Serve(listener)
-	})
+func (s *ExampleDelivererService) Stopped(info *ServerInfo) error {
+	return nil
 }
 
 type ExampleDelivererServer struct {
