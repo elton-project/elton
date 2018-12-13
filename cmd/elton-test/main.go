e194efe83922ecd2b7a56c13de4a67bf23d4c72c
yuuki <yuuki0xff@gmail.com>
2018-12-13 16:18:10 +0900

    remove old code

--- cmd/elton-test/main.go
+++ cmd/elton-test/main.go
@@ -7,53 +7,6 @@ import (
 	"time"
 )
 
-//// EventManagerのサーバのセットアップをする。
-//// サーバがlistenしているアドレスをemAddrに格納する。
-//// 返した関数を実行すると、リクエストを受け付ける状況になる。
-//func serveEventManager(ctx context.Context, emAddr *net.Addr) func() error {
-//	m := ServiceManager{
-//		Services: []Service{
-//			&ManagerService{
-//				L: zap.S(),
-//			},
-//		},
-//	}
-//	if err := m.Setup(ctx); err != nil {
-//		zap.S().Errorw("EventManager", "error", err)
-//		return func() error {
-//			return err
-//		}
-//	}
-//	*emAddr = m.Addrs()[0]
-//
-//	return func() error {
-//		if errs := m.Serve(ctx); len(errs) > 0 {
-//			zap.S().Errorw("EventManager", "errors", errs)
-//			return errs[0]
-//		}
-//		return nil
-//	}
-//}
-//func serveExampleSubsystem(ctx context.Context, emAddr net.Addr) func() error {
-//	ex := ExampleSubsystem{
-//		EMAddr: emAddr,
-//	}
-//	if err := ex.Setup(ctx); err != nil {
-//		zap.S().Errorw("ExampleSubsystem", "error", err)
-//		return func() error {
-//			return err
-//		}
-//	}
-//
-//	return func() error {
-//		if errs := ex.Serve(ctx); len(errs) > 0 {
-//			zap.S().Errorw("ExampleSubsystem", "errors", errs)
-//			return errs[0]
-//		}
-//		return nil
-//	}
-//}
-
 func Main() int {
 	logger, _ := zap.NewDevelopment()
 	zap.ReplaceGlobals(logger)
@@ -76,13 +29,6 @@ func Main() int {
 		//EMAddr: emAddr,
 	}
 
-	//var emAddr net.Addr
-	//var eg errgroup.Group
-	//eg.Go(serveEventManager(ctx, &emAddr))
-	//eg.Go(serveExampleSubsystem(ctx, emAddr))
-	//if err := eg.Wait(); err != nil {
-	//	return 1
-	//}
 	return 0
 }
 func main() {
