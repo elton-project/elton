933ab7d497ebe24bd5269779378889e9b377b936
yuuki <yuuki0xff@gmail.com>
2019-04-03 12:27:03 +0900

    register_filesystemに成功

--- kmod/simple_fs.c
+++ kmod/simple_fs.c
@@ -1,13 +1,67 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/dcache.h>
+
+#define MODULE_NAME "simple_fs"
+#define FS_NAME MODULE_NAME
+
+#define _PRINTLNK(level, fmt, ...) (printk(level MODULE_NAME ": " fmt "\n", ##__VA_ARGS__))
+#define DEBUG(fmt, ...) _PRINTLNK(KERN_DEBUG, fmt, ##__VA_ARGS__)
+#define INFO(fmt, ...) _PRINTLNK(KERN_INFO, fmt, ##__VA_ARGS__)
+#define ERR(fmt, ...) _PRINTLNK(KERN_ERR, fmt, ##__VA_ARGS__)
+
+// エラーならtrueを返す。
+// また、エラー発生時にログを残す。
+#define CHECK_ERROR(ret) ({ \
+	if(ret) { \
+		ERR("Occurred an error %d on %s (%s:%d)", ret, __func__, __FILE__, __LINE__); \
+	} \
+	ret; \
+})
+
+
+
+static int simplefs_fill_super(struct super_block *sb, void *data, int silent) {
+	return -ENOSYS;
+}
+static struct dentry *mount(struct file_system_type *fs_type,
+		int flags, const char *dev_name, void *data) {
+	return mount_nodev(fs_type, flags, data, simplefs_fill_super);
+}
+static void kill_sb(struct super_block *sb) {}
+
+
+static struct file_system_type simplefs_type = {
+	.name = "simple_fs",
+	.mount = mount,
+	.kill_sb = kill_sb,
+	.fs_flags = 0
+};
 
 static int __init fs_module_init(void) {
-    printk(KERN_INFO "sample module loaded\n");
-    return 0;
+	int ret;
+	DEBUG("Loading the module ...");
+
+	ret = register_filesystem(&simplefs_type);
+	if(CHECK_ERROR(ret)) {
+		return ret;
+	}
+
+	INFO("The module loaded");
+	return 0;
 }
 
 static void __exit fs_module_exit(void) {
-    printk(KERN_INFO "sample module unloaded\n");
+	int ret;
+	DEBUG("Unloading the module ...");
+
+	ret = unregister_filesystem(&simplefs_type);
+	if(CHECK_ERROR(ret)) {
+		return;
+	}
+
+	INFO("The module unloaded");
 }
 
 module_init(fs_module_init);
@@ -15,4 +69,4 @@ module_exit(fs_module_exit);
 
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("yuuki0xff <yuuki0xff@gmail.com>");
-MODULE_DESCRIPTION("sample module");
+MODULE_DESCRIPTION(MODULE_NAME " module");
