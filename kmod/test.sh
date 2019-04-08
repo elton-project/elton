ce494ee58efa92b885662ddb79d120557682866f
yuuki <yuuki0xff@gmail.com>
2019-04-08 17:55:07 +0900

    change test server ip address

--- kmod/test.sh
+++ kmod/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -euv
-SERVER=root@192.168.189.190
+SERVER=root@192.168.189.55
 DIR=samplefs-kmod
 
 rsync -av --del ./ $SERVER:$DIR/
--- kmod/view-log.sh
+++ kmod/view-log.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-SERVER=root@192.168.189.190
+SERVER=root@192.168.189.55
 
 ssh -t $SERVER dmesg --follow
