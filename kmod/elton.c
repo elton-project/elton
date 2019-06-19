aa1498925b08605c69a43d28e83969cc926fcae8
yuuki <yuuki0xff@gmail.com>
2019-06-19 20:24:58 +0900

    rename symbole names

--- kmod/elton.c
+++ kmod/elton.c
@@ -10,8 +10,8 @@
 
 #define MODULE_NAME "eltonfs"
 #define FS_NAME "elton"
-#define SIMPLEFS_SUPER_MAGIC 0x51891f5
-#define SIMPLEFS_NAME_LEN 255
+#define ELTONFS_SUPER_MAGIC 0x51891f5
+#define ELTONFS_NAME_LEN 255
 
 #define _PRINTLNK(level, fmt, ...) (printk(level MODULE_NAME ": " fmt "\n", ##__VA_ARGS__))
 #define DEBUG(fmt, ...) _PRINTLNK(KERN_DEBUG, fmt, ##__VA_ARGS__)
@@ -37,22 +37,22 @@
 
 
 static bool is_registered = 0;
-static struct file_system_type simplefs_type;
-static struct super_operations simplefs_s_op;
-static struct address_space_operations simplefs_aops;
-static struct inode_operations simplefs_file_inode_operations;
-static struct inode_operations simplefs_dir_inode_operations;
-static struct file_operations simplefs_file_operations;
+static struct file_system_type eltonfs_type;
+static struct super_operations eltonfs_s_op;
+static struct address_space_operations eltonfs_aops;
+static struct inode_operations eltonfs_file_inode_operations;
+static struct inode_operations eltonfs_dir_inode_operations;
+static struct file_operations eltonfs_file_operations;
 
-struct simplefs_info {
-#ifdef SIMPLEFS_STATISTIC
+struct eltonfs_info {
+#ifdef ELTONFS_STATISTIC
 	unsigned long mmap_size;
 	rwlock_t mmap_size_lock;
 #endif
 };
 
 
-static struct inode *simplefs_get_inode(struct super_block *sb,
+static struct inode *eltonfs_get_inode(struct super_block *sb,
 				const struct inode *dir, umode_t mode, dev_t dev) {
 	struct inode *inode;
 	inode = new_inode(sb);
@@ -62,7 +62,7 @@ static struct inode *simplefs_get_inode(struct super_block *sb,
 
 	inode->i_ino = get_next_ino();
 	inode_init_owner(inode, dir, mode);
-	inode->i_mapping->a_ops = &simplefs_aops;
+	inode->i_mapping->a_ops = &eltonfs_aops;
 	mapping_set_gfp_mask(inode->i_mapping, GFP_HIGHUSER);
 	mapping_set_unevictable(inode->i_mapping);
 	inode->i_atime = inode->i_mtime = inode->i_ctime = current_time(inode);
@@ -71,11 +71,11 @@ static struct inode *simplefs_get_inode(struct super_block *sb,
 		init_special_inode(inode, mode, dev);
 		break;
 	case S_IFREG:
-		inode->i_op = &simplefs_file_inode_operations;
-		inode->i_fop = &simplefs_file_operations;
+		inode->i_op = &eltonfs_file_inode_operations;
+		inode->i_fop = &eltonfs_file_operations;
 		break;
 	case S_IFDIR:
-		inode->i_op = &simplefs_dir_inode_operations;
+		inode->i_op = &eltonfs_dir_inode_operations;
 		inode->i_fop = &simple_dir_operations;
 
 		/* directory inodes start off with i_nlink == 2 (for "." entry) */
@@ -89,7 +89,7 @@ static struct inode *simplefs_get_inode(struct super_block *sb,
 	return inode;
 }
 
-static int simplefs_set_page_dirty(struct page *page) {
+static int eltonfs_set_page_dirty(struct page *page) {
 	if(PageDirty(page)) {
 		return 0;
 	}
@@ -97,8 +97,8 @@ static int simplefs_set_page_dirty(struct page *page) {
 	return 0;
 }
 
-static int simplefs_mknod(struct inode *dir, struct dentry *dentry, umode_t mode, dev_t dev) {
-	struct inode *inode = simplefs_get_inode(dir->i_sb, dir, mode, dev);
+static int eltonfs_mknod(struct inode *dir, struct dentry *dentry, umode_t mode, dev_t dev) {
+	struct inode *inode = eltonfs_get_inode(dir->i_sb, dir, mode, dev);
 	if(! inode) {
 		return -ENOSPC;
 	}
@@ -108,12 +108,12 @@ static int simplefs_mknod(struct inode *dir, struct dentry *dentry, umode_t mode
 	return 0;
 }
 
-static int simplefs_create(struct inode *dir, struct dentry *dentry, umode_t mode, bool excl) {
-	return simplefs_mknod(dir, dentry, mode | S_IFREG, 0);
+static int eltonfs_create(struct inode *dir, struct dentry *dentry, umode_t mode, bool excl) {
+	return eltonfs_mknod(dir, dentry, mode | S_IFREG, 0);
 }
 
-static int simplefs_mkdir(struct inode *dir, struct dentry *dentry, umode_t mode) {
-	int error = simplefs_mknod(dir, dentry, mode | S_IFDIR, 0);
+static int eltonfs_mkdir(struct inode *dir, struct dentry *dentry, umode_t mode) {
+	int error = eltonfs_mknod(dir, dentry, mode | S_IFDIR, 0);
 	if(error) {
 		return error;
 	}
@@ -121,11 +121,11 @@ static int simplefs_mkdir(struct inode *dir, struct dentry *dentry, umode_t mode
 	return 0;
 }
 
-static int simplefs_symlink(struct inode *dir, struct dentry *dentry, const char *symname) {
+static int eltonfs_symlink(struct inode *dir, struct dentry *dentry, const char *symname) {
 	struct inode *inode;
 	int len, error;
 
-	inode = simplefs_get_inode(dir->i_sb, dir, S_IFLNK | S_IRWXUGO, 0);
+	inode = eltonfs_get_inode(dir->i_sb, dir, S_IFLNK | S_IRWXUGO, 0);
 	if(! inode) {
 		return -ENOSPC;
 	}
@@ -141,11 +141,11 @@ static int simplefs_symlink(struct inode *dir, struct dentry *dentry, const char
 	return 0;
 }
 
-static unsigned long simplefs_get_unmapped_area(struct file *file, unsigned long addr, unsigned long len, unsigned long pgoff, unsigned long flags) {
+static unsigned long eltonfs_get_unmapped_area(struct file *file, unsigned long addr, unsigned long len, unsigned long pgoff, unsigned long flags) {
 	return current->mm->get_unmapped_area(file, addr, len, pgoff, flags);
 }
 
-static long simplefs_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
+static long eltonfs_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
 	struct inode *inode = file_inode(file);
 	unsigned int flags;
 
@@ -162,7 +162,7 @@ static long simplefs_ioctl(struct file *file, unsigned int cmd, unsigned long ar
 }
 
 #ifdef CONFIG_COMPAT
-static long simplefs_compat_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
+static long eltonfs_compat_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
 	switch(cmd) {
 		case FS_IOC32_GETFLAGS:
 			cmd = FS_IOC_GETFLAGS;
@@ -173,16 +173,16 @@ static long simplefs_compat_ioctl(struct file *file, unsigned int cmd, unsigned 
 		default:
 			return -ENOSYS;
 	}
-	return simplefs_ioctl(file, cmd, arg);
+	return eltonfs_ioctl(file, cmd, arg);
 }
 #endif
 
-static int simplefs_fill_super(struct super_block *sb, void *data, int silent) {
+static int eltonfs_fill_super(struct super_block *sb, void *data, int silent) {
 	struct inode *inode;
 	struct dentry *root;
 
-	struct simplefs_info *info = kmalloc(sizeof(struct simplefs_info), GFP_KERNEL);
-#ifdef SIMPLEFS_STATISTIC
+	struct eltonfs_info *info = kmalloc(sizeof(struct eltonfs_info), GFP_KERNEL);
+#ifdef ELTONFS_STATISTIC
 	rwlock_init(&info->mmap_size_lock);
 	info->mmap_size = 0;
 #endif
@@ -191,12 +191,12 @@ static int simplefs_fill_super(struct super_block *sb, void *data, int silent) {
 	sb->s_blocksize_bits = PAGE_SHIFT;
 	sb->s_blocksize = PAGE_SIZE;
 	sb->s_maxbytes = MAX_LFS_FILESIZE;
-	sb->s_type = &simplefs_type;
-	sb->s_op = &simplefs_s_op;
+	sb->s_type = &eltonfs_type;
+	sb->s_op = &eltonfs_s_op;
 	sb->s_time_gran = 1;
 	sb->s_fs_info = info;
 
-	inode = simplefs_get_inode(sb, NULL, S_IFDIR, 0);
+	inode = eltonfs_get_inode(sb, NULL, S_IFDIR, 0);
 	ASSERT_NOT_NULL(inode);
 	root = d_make_root(inode);
 	ASSERT_NOT_NULL(root);
@@ -206,11 +206,11 @@ static int simplefs_fill_super(struct super_block *sb, void *data, int silent) {
 }
 static struct dentry *mount(struct file_system_type *fs_type,
 		int flags, const char *dev_name, void *data) {
-	return mount_nodev(fs_type, flags, data, simplefs_fill_super);
+	return mount_nodev(fs_type, flags, data, eltonfs_fill_super);
 }
 static void kill_sb(struct super_block *sb) {}
 
-static int simplefs_statfs(struct dentry *dentry, struct kstatfs *buf) {
+static int eltonfs_statfs(struct dentry *dentry, struct kstatfs *buf) {
 	// TODO: ダミーデータではなく、本当の値を設定する。
 	int total_blocks = 10000;
 	int used_blocks = 1000;
@@ -219,7 +219,7 @@ static int simplefs_statfs(struct dentry *dentry, struct kstatfs *buf) {
 
 	struct kstatfs stat = {
 		/* Type of filesystem */
-		.f_type = SIMPLEFS_SUPER_MAGIC,
+		.f_type = ELTONFS_SUPER_MAGIC,
 		/* Optimal transfer block size */
 		.f_bsize = PAGE_SIZE,
 		/* Total data blocks in filesystem */
@@ -246,7 +246,7 @@ static int simplefs_statfs(struct dentry *dentry, struct kstatfs *buf) {
 }
 
 // Display the mount options in /proc/mounts.
-static int simplefs_show_options(struct seq_file *m, struct dentry *root) {
+static int eltonfs_show_options(struct seq_file *m, struct dentry *root) {
 	// seq_puts(m, ",default");
 	return 0;
 }
@@ -256,7 +256,7 @@ static int __init fs_module_init(void) {
 	int error;
 	DEBUG("Loading the module ...");
 
-	error = register_filesystem(&simplefs_type);
+	error = register_filesystem(&eltonfs_type);
 	if(CHECK_ERROR(error)) {
 		return error;
 	}
@@ -271,7 +271,7 @@ static void __exit fs_module_exit(void) {
 	DEBUG("Unloading the module ...");
 
 	if(is_registered) {
-		error = unregister_filesystem(&simplefs_type);
+		error = unregister_filesystem(&eltonfs_type);
 		if(CHECK_ERROR(error)) {
 			return;
 		}
@@ -280,9 +280,9 @@ static void __exit fs_module_exit(void) {
 	INFO("The module unloaded");
 }
 
-int simplefs_file_mmap(struct file *file, struct vm_area_struct *vma) {
-#ifdef SIMPLEFS_STATISTIC
-	struct simplefs_info *info = file->f_path.mnt->mnt_sb->s_fs_info;
+int eltonfs_file_mmap(struct file *file, struct vm_area_struct *vma) {
+#ifdef ELTONFS_STATISTIC
+	struct eltonfs_info *info = file->f_path.mnt->mnt_sb->s_fs_info;
 	unsigned long size = vma->vm_end - vma->vm_start;
 	int need_logging = 0;
 
@@ -301,30 +301,30 @@ int simplefs_file_mmap(struct file *file, struct vm_area_struct *vma) {
 }
 
 
-static struct file_system_type simplefs_type = {
+static struct file_system_type eltonfs_type = {
 	.owner = THIS_MODULE,
 	.name = FS_NAME,
 	.mount = mount,
 	.kill_sb = kill_sb,
 	.fs_flags = 0
 };
-static struct super_operations simplefs_s_op = {
-	.statfs		= simplefs_statfs,
+static struct super_operations eltonfs_s_op = {
+	.statfs		= eltonfs_statfs,
 	.drop_inode	= generic_delete_inode,
-	.show_options	= simplefs_show_options,
+	.show_options	= eltonfs_show_options,
 };
-static struct address_space_operations simplefs_aops = {
+static struct address_space_operations eltonfs_aops = {
 	.readpage	= simple_readpage,
 	.write_begin	= simple_write_begin,
 	.write_end	= simple_write_end,
-	.set_page_dirty	= simplefs_set_page_dirty,
+	.set_page_dirty	= eltonfs_set_page_dirty,
 };
-static struct inode_operations simplefs_file_inode_operations = {
+static struct inode_operations eltonfs_file_inode_operations = {
 	.setattr = simple_setattr,
 	.getattr = simple_getattr,
 };
-static struct inode_operations simplefs_dir_inode_operations = {
-	.create = simplefs_create,
+static struct inode_operations eltonfs_dir_inode_operations = {
+	.create = eltonfs_create,
 	// on-memory filesystemでは、保持しているファイルに対応するdentryは、必ず存在する。
 	// オンメモリファイルシステムでのlookupが呼び出されるタイミングは、存在しないファイル
 	// にアクセスしたときだけである。
@@ -334,24 +334,24 @@ static struct inode_operations simplefs_dir_inode_operations = {
 	.lookup = simple_lookup,
 	.link = simple_link,
 	.unlink = simple_unlink,
-	.symlink = simplefs_symlink,
-	.mkdir = simplefs_mkdir,
+	.symlink = eltonfs_symlink,
+	.mkdir = eltonfs_mkdir,
 	.rmdir = simple_rmdir,
-	.mknod = simplefs_mknod,
+	.mknod = eltonfs_mknod,
 	.rename = simple_rename,
 };
-static struct file_operations simplefs_file_operations = {
+static struct file_operations eltonfs_file_operations = {
 	.read_iter = generic_file_read_iter,
 	.write_iter = generic_file_write_iter,
-	.mmap = simplefs_file_mmap,
+	.mmap = eltonfs_file_mmap,
 	.fsync = noop_fsync,
 	.splice_read = generic_file_splice_read,
 	.splice_write = iter_file_splice_write,
 	.llseek = generic_file_llseek,
-	.get_unmapped_area = simplefs_get_unmapped_area,
-	.unlocked_ioctl = simplefs_ioctl,
+	.get_unmapped_area = eltonfs_get_unmapped_area,
+	.unlocked_ioctl = eltonfs_ioctl,
 #ifdef CONFIG_COMPAT
-	.compat_ioctl = simplefs_compat_ioctl,  // for 32bit application.  See https://qiita.com/akachochin/items/94ba679b2941f55c1d2d
+	.compat_ioctl = eltonfs_compat_ioctl,  // for 32bit application.  See https://qiita.com/akachochin/items/94ba679b2941f55c1d2d
 #endif
 };
 
