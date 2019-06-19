836cbbba12b5e8395a5868400b065b0ff212b8af
yuuki <yuuki0xff@gmail.com>
2019-06-19 16:18:01 +0900

    fix template

--- kmod/test/include/safe.h
+++ kmod/test/include/safe.h
@@ -2,7 +2,7 @@
 // Create a temporary file and return fd.
 #define SAFE_MKTEMP() \
 	({ \
-		char name[] = "/mnt/test_" TEST_NAME; \
+		char name[] = "/mnt/test_" TEST_NAME "XXXXXX"; \
 		int fd = CHECK_ERROR(mkstemp(name)); \
 		fd; \
 	})
