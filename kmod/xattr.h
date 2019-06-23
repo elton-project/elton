e76578928171ebf997d3b0c35fda2d0de09a9733
yuuki <yuuki0xff@gmail.com>
2019-06-24 04:53:53 +0900

    fix define

--- kmod/xattr.h
+++ kmod/xattr.h
@@ -8,7 +8,7 @@ ssize_t elton_list_xattr(struct inode *inode, char *buffer, size_t buffer_size);
 int elton_set_xattr(struct inode *inode, char *name, void *value, size_t size, int flags);
 int elton_get_xattr(struct inode *inode, char *name, void *value, size_t size);
 
-extern struct xattr_handler elton_xattr_user_handler;
+extern struct xattr_handler *elton_xattr_handlers[];
 
 
 #endif // _ELTON_XATTR_H
