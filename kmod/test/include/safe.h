429db69b2d5ecf5fa81ad7d11fde994237d4509d
yuuki <yuuki0xff@gmail.com>
2019-07-03 18:36:41 +0900

    add TEMP_BASE

--- kmod/test/include/safe.h
+++ kmod/test/include/safe.h
@@ -1,8 +1,12 @@
+// The base path for temporary file.
+#define TEMP_BASE "/mnt/test_" TEST_NAME "."
+// The template for temporary file path.
+#define TEMP_PATTERN TEMP_BASE "XXXXXX"
 
 // Create a temporary file and return fd.
 #define SAFE_MKTEMP() \
 	({ \
-		char name[] = "/mnt/test_" TEST_NAME "XXXXXX"; \
+		char name[] = TEMP_PATTERN; \
 		int fd = CHECK_ERROR(mkstemp(name)); \
 		fd; \
 	})
