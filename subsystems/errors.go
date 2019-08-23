fa43bc87a0d35c8f1113108d891fd4ce9f5b0827
yuuki <yuuki0xff@gmail.com>
2019-08-23 17:48:07 +0900

    add support for xerrors

--- subsystems/errors.go
+++ subsystems/errors.go
@@ -1,11 +1,31 @@
 package subsystems
 
-import "fmt"
+import (
+	"fmt"
+	werror "github.com/sonatard/werror/xerrors"
+	"golang.org/x/xerrors"
+)
 
 type RestartRequest struct {
+	werror.WrapError
 	Reason string
 }
 
-func (rr *RestartRequest) Error() string {
-	return fmt.Sprintf("RestartRequest: %s", rr.Reason)
+func NewRestartRequest(reason string) *RestartRequest {
+	err := &RestartRequest{
+		Reason: reason,
+	}
+	err.WrapError = werror.Wrap(err, nil, 2)
+	return err
+}
+func (e RestartRequest) Wrap(next error) error {
+	e.WrapError = werror.Wrap(&e, next, 2)
+	return &e
+}
+func (e *RestartRequest) Error() string {
+	return fmt.Sprintf("RestartRequest: %s", e.Reason)
+}
+func (e *RestartRequest) Is(err error) bool {
+	var other *RestartRequest
+	return xerrors.As(err, &other)
 }
