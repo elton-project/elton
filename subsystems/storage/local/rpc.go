b4992fc21b23f450002baae2bafba2fccb2391ac
yuuki <yuuki0xff@gmail.com>
2019-08-27 17:51:45 +0900

    fix return values count mismatch & fill GetObjectResponse.Info

--- subsystems/storage/local/rpc.go
+++ subsystems/storage/local/rpc.go
@@ -2,6 +2,7 @@ package localStorage
 
 import (
 	"context"
+	"github.com/golang/protobuf/ptypes"
 	elton_v2 "gitlab.t-lab.cs.teu.ac.jp/yuuki/elton/api/v2"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -38,11 +39,15 @@ func (s *StorageService) GetObject(ctx context.Context, req *elton_v2.GetObjectR
 		return nil, status.Errorf(codes.InvalidArgument, "key must not empty string")
 	}
 
-	body, err := s.Repo.Get(key, req.Offset, req.Size)
+	body, info, err := s.Repo.Get(key, req.Offset, req.Size)
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "local storage: failed to read the object: %s", err.Error())
 	}
 
+	createTime, err := ptypes.TimestampProto(info.CreateTime)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "local storage: failed to convert timestamp: %s", err.Error())
+	}
 	return &elton_v2.GetObjectResponse{
 		Key: &elton_v2.ObjectKey{
 			Id: key.ID,
@@ -50,7 +55,12 @@ func (s *StorageService) GetObject(ctx context.Context, req *elton_v2.GetObjectR
 		Body: &elton_v2.ObjectBody{
 			Contents: body,
 		},
-		Info: &elton_v2.ObjectInfo{}, // TODO
+		Info: &elton_v2.ObjectInfo{
+			Hash:          info.Hash,
+			HashAlgorithm: info.HashAlgorithm,
+			CreateTime:    createTime,
+			Size:          info.Size,
+		},
 	}, nil
 }
 func (s *StorageService) DeleteObject(ctx context.Context, req *elton_v2.DeleteObjectRequest) (*elton_v2.DeleteObjectResponse, error) {
