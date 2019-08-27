5534dd5f16e4c54b77b6021110fda487eaad9de3
yuuki <yuuki0xff@gmail.com>
2019-08-27 17:39:32 +0900

    fix test cases

--- subsystems/storage/local/repository_test.go
+++ subsystems/storage/local/repository_test.go
@@ -1,76 +1,130 @@
 package localStorage
 
 import (
-	"fmt"
+	"crypto/sha1"
 	"github.com/stretchr/testify/assert"
 	"github.com/yuuki0xff/pathlib"
 	"golang.org/x/xerrors"
 	"io/ioutil"
 	"os"
 	"testing"
+	"time"
 )
 
-func withTempRepo(fn func(repo *Repository)) {
+func withTempRepo(maxSize uint64, fn func(repo *Repository)) {
 	dir, err := ioutil.TempDir("", "")
 	if err != nil {
 		panic(err)
 	}
 	defer os.RemoveAll(dir)
 
-	repo := Repository{
-		BasePath: pathlib.New(dir),
-		MaxSize:  5,
-		KeyGen:   &RandomKeyGen{},
-	}
-	fn(&repo)
+	repo := NewRepository(pathlib.New(dir), nil, maxSize)
+	fn(repo)
+}
+func withTempRepoAndObject(maxSize uint64, objs [][]byte, fn func(repo *Repository, keys []Key)) {
+	withTempRepo(maxSize, func(repo *Repository) {
+		var keys []Key
+		for _, obj := range objs {
+			hash := sha1.Sum(obj)
+			info := Info{
+				Hash:          hash[:],
+				HashAlgorithm: "SHA1",
+				CreateTime:    time.Now(),
+				Size:          uint64(len(obj)),
+			}
+			key, err := repo.Create(obj, info)
+			if err != nil {
+				panic(err)
+			}
+			keys = append(keys, key)
+		}
+
+		fn(repo, keys)
+	})
 }
 
 func TestRepository_Create(t *testing.T) {
-	withTempRepo(func(repo *Repository) {
-		_, err := repo.Create([]byte("test"))
-		assert.NoError(t, err)
+	body := []byte("test")
+	hash := sha1.Sum(body)
+	info := Info{
+		Hash:          hash[:],
+		HashAlgorithm: "SHA1",
+		CreateTime:    time.Now(),
+		Size:          uint64(len(body)),
+	}
 
-		_, err = repo.Create([]byte("testt"))
-		assert.NoError(t, err)
-
-		_, err = repo.Create([]byte("testte"))
-		assert.Error(t, err, "body too big")
+	t.Run("normal-case", func(t *testing.T) {
+		withTempRepo(10, func(repo *Repository) {
+			_, err := repo.Create(body, info)
+			assert.NoError(t, err)
+		})
+	})
+	t.Run("max-length", func(t *testing.T) {
+		withTempRepo(uint64(len(body)), func(repo *Repository) {
+			_, err := repo.Create(body, info)
+			assert.NoError(t, err)
+		})
+	})
+	t.Run("size-over", func(t *testing.T) {
+		withTempRepo(1, func(repo *Repository) {
+			_, err := repo.Create(body, info)
+			assert.Error(t, err, "body too big")
+		})
+	})
+	t.Run("mismatch-size", func(t *testing.T) {
+		withTempRepo(10, func(repo *Repository) {
+			info2 := info
+			info2.Size = 1
+			_, err := repo.Create(body, info2)
+			assert.Error(t, err, "hoge")
+		})
+	})
+	t.Run("mismatch-hash", func(t *testing.T) {
+		withTempRepo(10, func(repo *Repository) {
+			info2 := info
+			info2.Hash = []byte("bla bla")
+			_, err := repo.Create(body, info2)
+			assert.Error(t, err, "hoge")
+		})
 	})
 }
 func TestRepository_Get(t *testing.T) {
-	withTempRepo(func(repo *Repository) {
-		key, err := repo.Create([]byte("test"))
-		assert.NoError(t, err)
-
-		// Get with the valid key.
-		b, err := repo.Get(key, 0, 0)
-		assert.Equal(t, b, []byte("test"))
-		assert.Nil(t, err)
-
-		// Get with invalid key.
-		b, err = repo.Get(Key{"invalid"}, 0, 0)
-		assert.Nil(t, b)
-		fmt.Println(err)
-		assert.True(t, xerrors.Is(err, &ObjectNotFoundError{}))
-
-		// Try to get an object with the not found key>
-		b, err = repo.Get(Key{"not found"}, 0, 0)
-		assert.Nil(t, b)
-		fmt.Println(err)
-		assert.True(t, xerrors.Is(err, &ObjectNotFoundError{}))
+	objs := [][]byte{
+		[]byte("test"),
+	}
+	t.Run("with-the-valid-key", func(t *testing.T) {
+		withTempRepoAndObject(10, objs, func(repo *Repository, keys []Key) {
+			body, info, err := repo.Get(keys[0], 0, 0)
+			assert.Nil(t, err)
+			assert.Equal(t, body, []byte("test"))
+			assert.NotEqual(t, 0, info.Size)
+		})
+	})
+	t.Run("not-found", func(t *testing.T) {
+		withTempRepoAndObject(10, objs, func(repo *Repository, keys []Key) {
+			body, info, err := repo.Get(Key{"not found"}, 0, 0)
+			assert.True(t, xerrors.Is(err, &ObjectNotFoundError{}))
+			assert.Nil(t, body)
+			assert.Empty(t, info)
+		})
 	})
 }
 func TestExists(t *testing.T) {
-	withTempRepo(func(repo *Repository) {
-		key, err := repo.Create([]byte("test"))
-		assert.NoError(t, err)
-
-		ok, err := repo.Exists(key)
-		assert.NoError(t, err)
-		assert.True(t, ok)
-
-		ok, err = repo.Exists(Key{"not found"})
-		assert.NoError(t, err)
-		assert.False(t, ok)
+	objs := [][]byte{
+		[]byte("test"),
+	}
+	t.Run("found", func(t *testing.T) {
+		withTempRepoAndObject(10, objs, func(repo *Repository, keys []Key) {
+			ok, err := repo.Exists(keys[0])
+			assert.NoError(t, err)
+			assert.True(t, ok)
+		})
+	})
+	t.Run("not-found", func(t *testing.T) {
+		withTempRepoAndObject(10, objs, func(repo *Repository, keys []Key) {
+			ok, err := repo.Exists(Key{"not found"})
+			assert.NoError(t, err)
+			assert.False(t, ok)
+		})
 	})
 }
