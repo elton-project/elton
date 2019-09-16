81619171d8e3870f4755e9b6a5d16a0e22911ba6
yuuki <yuuki0xff@gmail.com>
2019-09-16 21:05:18 +0900

    add test cases

--- subsystems/controller/db/local_test.go
+++ subsystems/controller/db/local_test.go
@@ -3,6 +3,7 @@ package controller_db
 import (
 	"github.com/stretchr/testify/assert"
 	. "gitlab.t-lab.cs.teu.ac.jp/yuuki/elton/api/v2"
+	"golang.org/x/xerrors"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -51,6 +52,75 @@ func TestLocalVS_Get(t *testing.T) {
 			assert.NotNil(t, info2)
 			assert.Nil(t, err)
 			assert.Equal(t, info, info2)
+		})
+	})
+}
+
+func TestLocalVS_Exists(t *testing.T) {
+	t.Run("should_return_true_when_access_exist_volume", func(t *testing.T) {
+		withLocalDB(t, func(vs VolumeStore, cs CommitStore) {
+			id, err := vs.Create(&VolumeInfo{
+				Name: "dummy",
+			})
+			if !assert.Nil(t, err) || !assert.NotNil(t, id) {
+				return
+			}
+
+			ok, err := vs.Exists(id)
+			assert.Nil(t, err)
+			assert.True(t, ok)
+		})
+	})
+	t.Run("should_return_false_when_access_not_exists_volume", func(t *testing.T) {
+		withLocalDB(t, func(vs VolumeStore, cs CommitStore) {
+			notExistsID := &VolumeID{
+				Id: "33221100",
+			}
+			ok, err := vs.Exists(notExistsID)
+			assert.Nil(t, err)
+			assert.False(t, ok)
+		})
+	})
+}
+func TestLocalVS_Delete(t *testing.T) {
+	// TODO
+}
+
+func TestLocalVS_Walk(t *testing.T) {
+	t.Run("should_not_callback_when_emtpy", func(t *testing.T) {
+		withLocalDB(t, func(vs VolumeStore, cs CommitStore) {
+			err := vs.Walk(func(id *VolumeID, info *VolumeInfo) error {
+				t.Error("callback function is called when walking the empty bucket")
+				return nil
+			})
+			assert.Nil(t, err)
+		})
+	})
+	t.Run("should_all_volumes_appeared_when_walking", func(t *testing.T) {
+		withLocalDB(t, func(vs VolumeStore, cs CommitStore) {
+			_, err := vs.Create(&VolumeInfo{Name: "vol-1"})
+			assert.Nil(t, err)
+			_, err = vs.Create(&VolumeInfo{Name: "vol-2"})
+			assert.Nil(t, err)
+			_, err = vs.Create(&VolumeInfo{Name: "vol-3"})
+			assert.Nil(t, err)
+
+			volumes := map[string]bool{
+				"vol-1": false,
+				"vol-2": false,
+				"vol-3": false,
+			}
+			err = vs.Walk(func(id *VolumeID, info *VolumeInfo) error {
+				name := info.GetName()
+				isAppeared, ok := volumes[name]
+				if !assert.True(t, ok) || !assert.False(t, isAppeared) {
+					return xerrors.New("something wrong")
+				}
+
+				volumes[name] = true
+				return nil
+			})
+			assert.Nil(t, err)
 		})
 	})
 }
