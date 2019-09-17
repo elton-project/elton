80db15d94728f10f3e8e5b5f7a545a6a5051567d
yuuki <yuuki0xff@gmail.com>
2019-09-17 19:36:48 +0900

    add SetListener() method to write test cases

--- subsystems/server.go
+++ subsystems/server.go
@@ -1,10 +1,14 @@
 package subsystems
 
-import "context"
+import (
+	"context"
+	"net"
+)
 
 type Server interface {
 	Name() string
 	Configure() error
 	Listen() error
+	SetListener(l net.Listener)
 	Serve(ctx context.Context) error
 }
