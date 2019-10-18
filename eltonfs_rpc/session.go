67a9e7c8fbd847b6792a3aee2c3fda92596468ef
yuuki <yuuki0xff@gmail.com>
2019-10-18 14:18:34 +0900

    impl clientNS.Send()/CLose() ...

--- eltonfs_rpc/session.go
+++ eltonfs_rpc/session.go
@@ -1,9 +1,19 @@
 package eltonfs_rpc
 
 import (
+	"bytes"
 	"gitlab.t-lab.cs.teu.ac.jp/yuuki/elton/utils"
 	"golang.org/x/xerrors"
 	"net"
+)
+
+type PacketFlag uint8
+
+const (
+	_ PacketFlag = 1 << iota // ignore first value (0) by assigning to blank identifier
+	CreateSessionFlag
+	CloseSessionFlag
+	ErrorSessionFlag
 )
 
 type ClientSession interface {
@@ -39,6 +49,7 @@ func NewClientSession(conn net.Conn) ClientSession {
 	r := utils.WrapMustReader(conn)
 	return &clientS{
 		Conn: conn,
+		W:    w,
 		Enc:  NewXDREncoder(w),
 		Dec:  NewXDRDecoder(r),
 	}
@@ -46,6 +57,7 @@ func NewClientSession(conn net.Conn) ClientSession {
 
 type clientS struct {
 	Conn net.Conn
+	W    utils.MustWriter
 	Enc  XDREncoder
 	Dec  XDRDecoder
 
@@ -104,16 +116,82 @@ func (s *clientS) New() (ClientNS, error) {
 	return ns, nil
 }
 func (s *clientS) Close() error { panic("todo") }
+func (s *clientS) sendPacket(nsid uint64, flags PacketFlag, data interface{}) error {
+	buf := &bytes.Buffer{}
+	enc := NewXDREncoder(utils.WrapMustWriter(buf))
+	enc.Struct(data)
+	size := uint64(buf.Len())
+	sid := uint64(0) // StructID
+
+	err := HandlePanic(func() error {
+		s.Enc.Uint64(size)
+		s.Enc.Uint64(nsid)
+		s.Enc.Uint8(uint8(flags))
+		s.Enc.Uint64(sid)
+		s.W.MustWriteAll(buf.Bytes())
+		return nil
+	})
+	if err != nil {
+		return xerrors.Errorf("sendPacket: %w", err)
+	}
+	return nil
+}
 
 type clientNS struct {
 	S    *clientS
 	NSID uint64
+
+	established bool
+	closedC2S   bool
+	closedS2C   bool
 }
 
-func (ns *clientNS) Send(v interface{}) error                    { panic("todo") }
-func (ns *clientNS) SendErr(err *SessionError) error             { panic("todo") }
+func (ns *clientNS) Send(v interface{}) error {
+	if ns.closedS2C {
+		return xerrors.Errorf("the nested session (NSID=%d) is already closed", ns.NSID)
+	}
+
+	var flags PacketFlag
+	if !ns.established {
+		flags |= CreateSessionFlag
+	}
+	return ns.S.sendPacket(ns.NSID, flags, v)
+}
+func (ns *clientNS) SendErr(err *SessionError) error {
+	if ns.closedS2C {
+		return xerrors.Errorf("the nested session (NSID=%d) is already closed", ns.NSID)
+	}
+
+	var flags PacketFlag
+	if !ns.established {
+		flags |= CreateSessionFlag
+	}
+	flags |= ErrorSessionFlag
+	return ns.S.sendPacket(ns.NSID, flags, err)
+}
 func (ns *clientNS) Recv(empty interface{}) (interface{}, error) { panic("todo") }
-func (ns *clientNS) CloseWithError(err SessionError) error       { panic("todo") }
-func (ns *clientNS) Close() error                                { panic("todo") }
-func (ns *clientNS) IsSendable() bool                            { panic("todo") }
-func (ns *clientNS) IsReceivable() bool                          { panic("todo") }
+func (ns *clientNS) CloseWithError(err SessionError) error {
+	if !ns.established {
+		return xerrors.Errorf("the nested session (NSID=%d) is not established", ns.NSID)
+	}
+	if ns.closedC2S {
+		return xerrors.Errorf("the nested session (NSID=%d) is already closed", ns.NSID)
+	}
+
+	return ns.S.sendPacket(ns.NSID, CloseSessionFlag|ErrorSessionFlag, err)
+}
+func (ns *clientNS) Close() error {
+	if !ns.established {
+		return xerrors.Errorf("the nested session (NSID=%d) is not established", ns.NSID)
+	}
+	if ns.closedC2S {
+		return xerrors.Errorf("the nested session (NSID=%d) is already closed", ns.NSID)
+	}
+	return ns.S.sendPacket(ns.NSID, CloseSessionFlag, nil)
+}
+func (ns *clientNS) IsSendable() bool {
+	return ns.closedC2S
+}
+func (ns *clientNS) IsReceivable() bool {
+	return ns.established && !ns.closedS2C
+}
