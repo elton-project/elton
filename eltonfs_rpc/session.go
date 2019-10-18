b696ae3c0663976388a35c9339a78cc46927a955
yuuki <yuuki0xff@gmail.com>
2019-10-18 12:31:03 +0900

    impl Setup()

--- eltonfs_rpc/session.go
+++ eltonfs_rpc/session.go
@@ -1,6 +1,8 @@
 package eltonfs_rpc
 
 import (
+	"gitlab.t-lab.cs.teu.ac.jp/yuuki/elton/utils"
+	"golang.org/x/xerrors"
 	"net"
 )
 
@@ -32,11 +34,42 @@ type ClientNS interface {
 	IsReceivable() bool
 }
 
-type clientS struct {
-	Conn net.Conn
+func NewClientSession(conn net.Conn) ClientSession {
+	w := utils.WrapMustWriter(conn)
+	r := utils.WrapMustReader(conn)
+	return &clientS{
+		Conn: conn,
+		Enc:  NewXDREncoder(w),
+		Dec:  NewXDRDecoder(r),
+	}
 }
 
-func (s *clientS) Setup() error  { panic("todo") }
+type clientS struct {
+	Conn net.Conn
+	Enc  XDREncoder
+	Dec  XDRDecoder
+
+	setupOK bool
+}
+
+func (s *clientS) Setup() error {
+	return HandlePanic(func() error {
+		s.Enc.Struct(&Setup1{
+			ClientName: "eltonfs-helper",
+		})
+		s2 := s.Dec.Struct(&Setup2{}).(*Setup2)
+		if s2.Error != 0 {
+			return xerrors.Errorf(
+				"%d - %s: %s",
+				s2.Error,
+				ErrorID(s2.Error).String(),
+				s2.Reason,
+			)
+		}
+		s.setupOK = true
+		return nil
+	})
+}
 func (s *clientS) New() ClientNS { panic("todo") }
 func (s *clientS) Close() error  { panic("todo") }
 
