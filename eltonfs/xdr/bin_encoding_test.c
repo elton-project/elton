66e543be03b1944d7cba99f24468a895e9f0eb44
yuuki <yuuki0xff@gmail.com>
2019-10-24 20:59:20 +0900

    add test case for bin encoder/decoder

---
+++ eltonfs/xdr/bin_encoding_test.c
@@ -0,0 +1,21 @@
+#ifndef ELTONFS_UNIT_TEST
+#include "interface.h"
+#include <assert.h>
+
+int test_encode_u8() {
+    struct xdr_encoder enc;
+    char buff[4] = {0, 0, 0, 99};
+    int len = 3;
+    int err;
+
+    err = default_encoder_init(&enc, buff, len);
+    if(err) return err;
+
+    CHECK_ERROR(enc.enc_op->u8(&enc, 1));
+    CHECK_ERROR(enc.enc_op->u8(&enc, 2));
+    CHECK_ERROR(enc.enc_op->u8(&enc, 3));
+    enc.enc_op->u8(&enc, 4);
+    return 0;
+}
+
+#endif // ELTONFS_UNIT_TEST
