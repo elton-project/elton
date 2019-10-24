1d51e68d0117989d745accd1b5fa479891a2f219
yuuki <yuuki0xff@gmail.com>
2019-10-24 15:36:16 +0900

    use io.Pipe to prevent unexpected io.EOF error

--- eltonfs_rpc/session_test.go
+++ eltonfs_rpc/session_test.go
@@ -4,42 +4,39 @@ import (
 	"bytes"
 	"github.com/stretchr/testify/assert"
 	"gitlab.t-lab.cs.teu.ac.jp/yuuki/elton/utils"
-	"golang.org/x/xerrors"
+	"io"
+	"io/ioutil"
 	"net"
 	"testing"
 	"time"
 )
 
 // newClientSession returns a session that it has been set up.
-func newClientSession() (*fakeConn, ClientSession) {
-	conn := &fakeConn{}
+func newClientSession() (server *fakeConn, s ClientSession) {
+	var client *fakeConn
+	client, server = newFakeConn()
 
 	// Prepare server response (Setup2).
-	enc := NewXDREncoder(utils.WrapMustWriter(&conn.ReadBuf))
+	enc := NewXDREncoder(utils.WrapMustWriter(server))
 	enc.Struct(&Setup2{
 		Error:      0,
 		Reason:     "",
 		ServerName: "eltonfs",
 	})
 
-	s := NewClientSession(conn)
+	s = NewClientSession(client)
 	err := s.Setup()
 	if err != nil {
 		panic(err)
 	}
-	if len(conn.ReadBuf.Bytes()) != 0 {
-		panic(xerrors.Errorf("partial read of Setup2 struct: remaining bytes are %d", len(conn.ReadBuf.Bytes())))
-	}
-	conn.ReadBuf.Reset()
-	conn.WriteBuf.Reset()
-	return conn, s
+	return
 }
 
 func TestClientS_Setup(t *testing.T) {
 	t.Run("no_error", func(t *testing.T) {
 		// Prepare server response (Setup2).
-		conn := &fakeConn{}
-		enc := NewXDREncoder(utils.WrapMustWriter(&conn.ReadBuf))
+		client, server := newFakeConn()
+		enc := NewXDREncoder(utils.WrapMustWriter(server))
 		enc.Struct(&Setup2{
 			Error:      0,
 			Reason:     "",
@@ -52,7 +49,7 @@ func TestClientS_Setup(t *testing.T) {
 		})
 
 		// Run the setup.
-		s := NewClientSession(conn)
+		s := NewClientSession(client)
 		defer s.Close()
 		err := s.Setup()
 		assert.NoError(t, err)
@@ -61,33 +58,29 @@ func TestClientS_Setup(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t,
 			req.Bytes(),
-			conn.WriteBuf.Bytes(),
+			server.MustReadAll(),
 		)
 	})
 	t.Run("invalid response", func(t *testing.T) {
 		// Prepare invalid server response (Setup2).
-		conn := &fakeConn{}
-		conn.ReadBuf.Write([]byte("invalid response"))
+		client, server := newFakeConn()
+		utils.WrapMustWriter(server).MustWriteAll([]byte("invalid response"))
 
+		s := NewClientSession(client)
+		defer client.Close()
 		// Run the setup.
-		s := NewClientSession(conn)
-		defer s.Close()
 		err := s.Setup()
 		assert.Error(t, err)
 	})
 }
 func TestClientS_New(t *testing.T) {
 	t.Run("send and recv", func(t *testing.T) {
-		conn, s := newClientSession()
+		server, s := newClientSession()
 		defer s.Close()
 
 		ns, err := s.New()
 		assert.NoError(t, err)
 		assert.NotNil(t, ns)
-		assert.Equal(t, []byte(nil), conn.WriteBuf.Bytes())
-
-		err = ns.Send(&Ping{})
-		assert.NoError(t, err)
 		expected := func() []byte {
 			// Prepare expected packet
 			buf := &bytes.Buffer{}
@@ -99,10 +92,10 @@ func TestClientS_New(t *testing.T) {
 			enc.Uint8(0)                        // Number of fields in struct.
 			return buf.Bytes()
 		}()
-		assert.Equal(t, expected, conn.WriteBuf.Bytes())
+		assert.Equal(t, expected, server.MustReadAll())
 
 		go func() {
-			enc := NewXDREncoder(utils.WrapMustWriter(&conn.ReadBuf))
+			enc := NewXDREncoder(utils.WrapMustWriter(server))
 			enc.Uint64(1)         // Number of data size in bytes.
 			enc.Uint64(1<<63 | 1) // SessionID
 			enc.Uint8(0)          // Flags
@@ -122,26 +115,34 @@ func TestClientS_New(t *testing.T) {
 	})
 }
 
+func newFakeConn() (*fakeConn, *fakeConn) {
+	r1, w1 := io.Pipe()
+	r2, w2 := io.Pipe()
+
+	return &fakeConn{
+			r: r1,
+			w: w2,
+		}, &fakeConn{
+			r: r2,
+			w: w1,
+		}
+}
+
 type fakeConn struct {
-	ReadBuf     bytes.Buffer
-	WriteBuf    bytes.Buffer
-	writeClosed bool
+	r *io.PipeReader
+	w *io.PipeWriter
 }
 
 func (f *fakeConn) Read(b []byte) (n int, err error) {
-	return f.ReadBuf.Read(b)
+	return f.r.Read(b)
 }
 
 func (f *fakeConn) Write(b []byte) (n int, err error) {
-	if f.writeClosed {
-		return 0, xerrors.New("write on closed conn")
-	}
-	return f.WriteBuf.Write(b)
+	return f.w.Write(b)
 }
 
 func (f *fakeConn) Close() error {
-	f.writeClosed = true
-	return nil
+	return f.w.Close()
 }
 
 func (f *fakeConn) LocalAddr() net.Addr {
@@ -162,4 +163,11 @@ func (f *fakeConn) SetReadDeadline(t time.Time) error {
 
 func (f *fakeConn) SetWriteDeadline(t time.Time) error {
 	panic("implement me")
+}
+func (f *fakeConn) MustReadAll() []byte {
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		panic(err)
+	}
+	return b
 }
