a9a6b05339418cd226b1ef3b96626d4fc27759e9
yuuki <yuuki0xff@gmail.com>
2019-10-29 14:27:27 +0900

    add comments

--- eltonfs/include/elton/xdr/bin_encoding.h
+++ eltonfs/include/elton/xdr/bin_encoding.h
@@ -4,7 +4,11 @@
 #include <elton/xdr/interface.h>
 #include <linux/types.h>
 
+// Initialize simple binary encoer.  See help of default_encoder_init() about
+// args.
 int bin_encoder_init(struct xdr_encoder *enc, char *buff, size_t len);
+// Initialize simple binary decoder.  See help of default_decoder_init() about
+// args.
 int bin_decoder_init(struct xdr_decoder *dec, char *buff, size_t len);
 
 #ifdef ELTONFS_UNIT_TEST
--- eltonfs/include/elton/xdr/interface.h
+++ eltonfs/include/elton/xdr/interface.h
@@ -4,30 +4,49 @@
 #include <linux/types.h>
 
 struct xdr_encoder {
+  // Buffer to write encoded data.  If NULL, the encoder only count required
+  // memory size for encoding it.
   char *buffer;
+  // Next write position in buffer.
   size_t pos;
+  // Size of buffer.  Should be zero if buffer is NULL.
   size_t len;
   struct xdr_encoder_operations *enc_op;
 };
 struct xdr_decoder {
+  // Buffer to read encoded data.  This field MUST NOT NULL.
   char *buffer;
+  // Next read position in buffer.
   size_t pos;
+  // Size of buffer.
   size_t len;
   struct xdr_decoder_operations *dec_op;
 };
 
 struct xdr_encoder_operations {
+  // Encode an unsigned 8bit integer.
   int (*u8)(struct xdr_encoder *enc, u8 val);
+  // Encode an unsigned 64bit integer.
   int (*u64)(struct xdr_encoder *enc, u64 val);
+  // Encode bytes.
   int (*bytes)(struct xdr_encoder *enc, char *bytes, size_t len);
 };
 struct xdr_decoder_operations {
+  // Deocde an unsigned 8bit integer.  If val is NULL, discard encoded data.
   int (*u8)(struct xdr_decoder *dec, u8 *val);
+  // Decode an unsigned 64bit integer.  If val is NULL, discard encoded data.
   int (*u64)(struct xdr_decoder *dec, u64 *val);
+  // Decode bytes.  If bytes is NULL, only the byte length is stored to len and
+  // the discard encoded data.
   int (*bytes)(struct xdr_decoder *dec, char *bytes, size_t *len);
 };
 
+// Initialize default encoder.
+// @buffer: Buffer.  If you need to the encoder only calculate required memory
+//          size, it should be NULL.
+// @len:    Length of buffer.  If buffer is NULL, it shoud be zero.
 int default_encoder_init(struct xdr_encoder *enc, char *buff, size_t len);
+// Initialize default decoder.
 int default_decoder_init(struct xdr_decoder *dec, char *buff, size_t len);
 
 #endif // _ELTON_XDR_INTERFACE_H
