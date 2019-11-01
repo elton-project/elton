3fc0bb0e35eecec5aaf99e8a22b0429b3e3bb3dd
yuuki <yuuki0xff@gmail.com>
2019-11-01 18:02:20 +0900

    fix typo

--- eltonfs/include/elton/rpc/packet.h
+++ eltonfs/include/elton/rpc/packet.h
@@ -41,7 +41,7 @@ int elton_rpc_decode_packet(struct raw_packet *in, void **out);
 void elton_rpc_free_decoded_data(void *data);
 // Get byte size of the whole of next raw_packet packet.
 // If need more bytes to calculate byte size, returns -ELTON_XDR_NEED_MORE_MEM.
-int elton_rpc_get_raw_packet_size(char *buff, size_t len, size_t *packed_size);
+int elton_rpc_get_raw_packet_size(char *buff, size_t len, size_t *packet_size);
 // Build raw_packet from bytes.
 int elton_rpc_build_raw_packet(struct raw_packet **out, char *buff, size_t len,
                                size_t *consumed);
