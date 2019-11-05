67713141304d91290c7d5c193ae319d59b23d4a8
yuuki <yuuki0xff@gmail.com>
2019-11-05 12:04:03 +0900

    start eltonfs_rpc client

--- cmd/eltonfs-helper/server.go
+++ cmd/eltonfs-helper/server.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"context"
-	"encoding/binary"
+	"gitlab.t-lab.cs.teu.ac.jp/yuuki/elton/eltonfs_rpc"
+	"golang.org/x/xerrors"
 	"net"
 )
 
 func handle(ctx context.Context, conn net.Conn) error {
 	defer conn.Close()
 
-	body := []byte("hello")
-	size := make([]byte, 8)
-	binary.BigEndian.PutUint64(size, uint64(len(body)))
-
-	conn.Write(size)
-	conn.Write(body)
-	// TODO: send/recv data
+	s := eltonfs_rpc.NewClientSession(conn)
+	defer s.Close()
+	if err := s.Setup(); err != nil {
+		return xerrors.Errorf("server: %w", err)
+	}
+	// TODO: wait for context finished or closed.
 	return nil
 }
