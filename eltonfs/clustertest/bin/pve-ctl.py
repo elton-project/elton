c8d945607f6200ae17bb93f4f403a985d2da8e00
yuuki <yuuki0xff@gmail.com>
2019-11-11 16:29:35 +0900

    enable wget timestamping feature

--- eltonfs/clustertest/bin/pve-ctl.py
+++ eltonfs/clustertest/bin/pve-ctl.py
@@ -289,8 +289,7 @@ class TemplateBuilder(typing.NamedTuple):
     def _set_storage(self, vm: VM):
         ip = Node(name=vm.node).ip
         # Download latest image.
-        RemoteCommand(ip=ip, cmd=['rm', '-f', UBUNTU_IMAGE_PATH, ]).execute()
-        RemoteCommand(ip=ip, cmd=['wget', UBUNTU_IMAGE_URL, '-O', UBUNTU_IMAGE_PATH]).execute()
+        RemoteCommand(ip=ip, cmd=['wget', UBUNTU_IMAGE_URL, '-O', UBUNTU_IMAGE_PATH, '-N']).execute()
 
         # Set dist to the VM.
         RemoteCommand(ip=ip, cmd=['qm', 'importdisk', str(vm.vmid), UBUNTU_IMAGE_PATH, STORAGE, '--format', 'qcow2']
