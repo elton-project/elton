92507c22ccb840615e3c4f3f27c461f1a11c2f47
yuuki <yuuki0xff@gmail.com>
2019-11-12 16:14:27 +0900

    change error codes

--- eltonfs/include/elton/error.h
+++ eltonfs/include/elton/error.h
@@ -1,14 +1,14 @@
 #ifndef _ELTON_ERROR_H
 #define _ELTON_ERROR_H
 
-#define ELTON_RPC_INVAL 1
-#define ELTON_RPC_BAD_PACKET 2
-#define ELTON_RPC_ALREADY_CLOSED 3
+#define ELTON_RPC_INVAL 1001
+#define ELTON_RPC_BAD_PACKET 1002
+#define ELTON_RPC_ALREADY_CLOSED 1003
 // Actual struct_id does not match the expected struct_id.
 #define ELTON_RPC_DIFF_TYPE 4
 
-#define ELTON_XDR_INVAL 1
-#define ELTON_XDR_NOMEM 2
-#define ELTON_XDR_NEED_MORE_MEM 3
+#define ELTON_XDR_INVAL 2001
+#define ELTON_XDR_NOMEM 2002
+#define ELTON_XDR_NEED_MORE_MEM 2003
 
 #endif // _ELTON_ERROR_H
