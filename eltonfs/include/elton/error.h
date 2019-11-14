7a2b461cb8ab8bba77cb4b73a8d5fecc1fb9163d
yuuki <yuuki0xff@gmail.com>
2019-11-14 19:50:29 +0900

    fix error code

--- eltonfs/include/elton/error.h
+++ eltonfs/include/elton/error.h
@@ -5,7 +5,7 @@
 #define ELTON_RPC_BAD_PACKET 1002
 #define ELTON_RPC_ALREADY_CLOSED 1003
 // Actual struct_id does not match the expected struct_id.
-#define ELTON_RPC_DIFF_TYPE 4
+#define ELTON_RPC_DIFF_TYPE 1004
 
 #define ELTON_XDR_INVAL 2001
 #define ELTON_XDR_NOMEM 2002
