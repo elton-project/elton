73e999b66c2fe304d9d62bd32b0dae3eeadb0626
yuuki <yuuki0xff@gmail.com>
2019-11-18 00:26:58 +0900

    print address of raw

--- eltonfs/rpc/server_session.c
+++ eltonfs/rpc/server_session.c
@@ -87,8 +87,8 @@ static int rpc_session_enqueue_raw_packet(struct elton_rpc_session *s,
     ADD_NS_NOLOCK(ns);
     ns = NULL;
   } else {
-    ERR("ns not found: ns=%p, nsid=%llu, flags=%hhu, struct_id=%llu", ns,
-        raw->session_id, raw->flags, raw->struct_id);
+    ERR("ns not found: ns=%p, raw=%p, nsid=%llu, flags=%hhu, struct_id=%llu",
+        ns, raw, raw->session_id, raw->flags, raw->struct_id);
     BUG();
     // Unreachable
     return -ENOTRECOVERABLE;
