c91ed939997a409bbf85a61de414397d7adb8646
yuuki <yuuki0xff@gmail.com>
2019-11-25 18:41:15 +0900

    retry net.Dial()

--- cmd/eltonfs-helper/root.go
+++ cmd/eltonfs-helper/root.go
@@ -4,10 +4,17 @@ import (
 	"context"
 	"github.com/spf13/cobra"
 	"golang.org/x/xerrors"
+	"log"
 	"net"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
+)
+
+const (
+	RpcServerRetryInterval = 100 * time.Millisecond
+	RpcServerRetryTimeout  = 3 * time.Second
 )
 
 var rootCmd = &cobra.Command{
@@ -32,7 +39,7 @@ func rootFn(cmd *cobra.Command, args []string) error {
 		return xerrors.Errorf("socket args: %w", err)
 	}
 
-	conn, err := net.Dial("unix", sock)
+	conn, err := tryConnect("unix", sock, RpcServerRetryInterval, RpcServerRetryTimeout, ctx)
 	if err != nil {
 		return xerrors.Errorf("connect: %w", err)
 	}
@@ -41,6 +48,30 @@ func rootFn(cmd *cobra.Command, args []string) error {
 		return xerrors.Errorf("handle: %w", err)
 	}
 	return nil
+}
+
+// tryConnect creates a connection to eltonfs in-kernel RPC server.
+// The net.Dial() may be fail depending on the initialization timing.  It retries net.Dial() to create a connection certainly.
+func tryConnect(network, addr string, interval time.Duration, timeout time.Duration, ctx context.Context) (net.Conn, error) {
+	var lastErr error
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+	timeoutCtx, _ := context.WithTimeout(ctx, timeout)
+
+	for i := 0; ; i++ {
+		conn, err := net.Dial(network, addr)
+		if err == nil {
+			return conn, nil
+		}
+		lastErr = xerrors.Errorf("tryConnect: %w", err)
+		log.Printf("failed to connect RPC server.  Retry after %s", interval.String())
+
+		select {
+		case <-ticker.C:
+		case <-timeoutCtx.Done():
+			return nil, lastErr
+		}
+	}
 }
 
 func cancelWhenSignal(ctx context.Context, cancel func()) {
