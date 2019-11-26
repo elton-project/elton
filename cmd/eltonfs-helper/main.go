37444215af956f6143a886526d1cc8675ed93e5e
yuuki <yuuki0xff@gmail.com>
2019-11-26 15:10:43 +0900

    send logs to syslog by default

--- cmd/eltonfs-helper/main.go
+++ cmd/eltonfs-helper/main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"golang.org/x/xerrors"
 	"log"
+	"log/syslog"
 	"os"
 )
 
 func Main() (exitCode int) {
+	// Change log output direction to syslog.
+	w, err := syslog.New(syslog.LOG_USER|syslog.LOG_INFO, "eltonfs-helper")
+	if err != nil {
+		// Write error log to stderr.
+		log.Printf("%+v\n", xerrors.Errorf("initializing syslog writer: %w", err))
+		return 2
+	}
+	defer w.Close()
+	log.SetOutput(w)
+	log.SetFlags(log.Llongfile)
+
 	log.Println("starting eltonfs-helper")
 	defer log.Printf("stopping eltonfs-helper with exit-code(%d)", exitCode)
 
