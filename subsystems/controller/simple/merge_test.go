6784403afd02a1bd21734dbf527669ee65882c4a
yuuki <yuuki0xff@gmail.com>
2019-12-13 19:58:11 +0900

    add test cases for filterNotModifiedInodes()

---
+++ subsystems/controller/simple/merge_test.go
@@ -0,0 +1,137 @@
+package simple
+
+import (
+	"github.com/deckarep/golang-set"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/golang/protobuf/ptypes/timestamp"
+	. "gitlab.t-lab.cs.teu.ac.jp/yuuki/elton/api/v2"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func mustProtoTime(t time.Time) *timestamp.Timestamp {
+	ts, err := ptypes.TimestampProto(t)
+	if err != nil {
+		panic(err)
+	}
+	return ts
+}
+
+func TestMerger_filterNotModifiedInodes(t *testing.T) {
+	type fields struct {
+		Info    *CommitInfo
+		Base    *Tree
+		Latest  *Tree
+		Current *Tree
+	}
+	type args struct {
+		inodes mapset.Set
+		base   *Tree
+		other  *Tree
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   mapset.Set
+	}{
+		{
+			name: "inogre_inodes_not_included_in_inodes_args",
+			args: args{
+				inodes: mapset.NewThreadUnsafeSetFromSlice([]interface{}{
+					uint64(1),
+				}),
+				base: &Tree{
+					Inodes: map[uint64]*File{
+						1: {FileType: FileType_Regular, Mode: 0644},
+						2: {FileType: FileType_Regular, Mode: 0644},
+					},
+				},
+				other: &Tree{
+					Inodes: map[uint64]*File{
+						1: {FileType: FileType_Regular, Mode: 0644},
+						2: {FileType: FileType_Regular, Mode: 0777},
+					},
+				},
+			},
+			want: mapset.NewThreadUnsafeSetFromSlice([]interface{}{}),
+		}, {
+			name: "ignore_mtime_changes_of_directories",
+			args: args{
+				inodes: mapset.NewThreadUnsafeSetFromSlice([]interface{}{
+					uint64(1),
+					uint64(2),
+					uint64(3),
+				}),
+				base: &Tree{
+					Inodes: map[uint64]*File{
+						1: {
+							FileType: FileType_Directory,
+							Atime:    mustProtoTime(time.Unix(10, 20)),
+							Mtime:    mustProtoTime(time.Unix(30, 40)),
+							Ctime:    mustProtoTime(time.Unix(50, 60)),
+							Entries:  map[string]uint64{},
+						},
+						2: {
+							FileType: FileType_Directory,
+							Atime:    mustProtoTime(time.Unix(10, 20)),
+							Mtime:    mustProtoTime(time.Unix(30, 40)),
+							Ctime:    mustProtoTime(time.Unix(50, 60)),
+							Entries:  map[string]uint64{},
+						},
+						3: {
+							FileType: FileType_Directory,
+							Atime:    mustProtoTime(time.Unix(10, 20)),
+							Mtime:    mustProtoTime(time.Unix(30, 40)),
+							Ctime:    mustProtoTime(time.Unix(50, 60)),
+							Entries:  map[string]uint64{},
+						},
+					},
+				},
+				other: &Tree{
+					Inodes: map[uint64]*File{
+						1: {
+							FileType: FileType_Directory,
+							Atime:    mustProtoTime(time.Unix(99, 99)), // Changed
+							Mtime:    mustProtoTime(time.Unix(30, 40)),
+							Ctime:    mustProtoTime(time.Unix(50, 60)),
+							Entries:  map[string]uint64{},
+						},
+						2: {
+							FileType: FileType_Directory,
+							Atime:    mustProtoTime(time.Unix(10, 20)),
+							Mtime:    mustProtoTime(time.Unix(99, 99)), // Changed
+							Ctime:    mustProtoTime(time.Unix(50, 60)),
+							Entries:  map[string]uint64{},
+						},
+						3: {
+							FileType: FileType_Directory,
+							Atime:    mustProtoTime(time.Unix(10, 20)),
+							Mtime:    mustProtoTime(time.Unix(30, 40)),
+							Ctime:    mustProtoTime(time.Unix(99, 99)), // Changed
+							Entries:  map[string]uint64{},
+						},
+					},
+				},
+			},
+			want: mapset.NewThreadUnsafeSetFromSlice([]interface{}{
+				uint64(1),
+				uint64(3),
+			}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &Merger{
+				Info:    tt.fields.Info,
+				Base:    tt.fields.Base,
+				Latest:  tt.fields.Latest,
+				Current: tt.fields.Current,
+			}
+			if got := m.filterNotModifiedInodes(tt.args.inodes, tt.args.base, tt.args.other); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("filterNotModifiedInodes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
