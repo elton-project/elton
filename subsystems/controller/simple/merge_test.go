73171794fa61ab8b82e4581724130edc96a8f611
yuuki <yuuki0xff@gmail.com>
2019-12-13 20:36:40 +0900

    add test cases for Merger.reverseIndex()

--- subsystems/controller/simple/merge_test.go
+++ subsystems/controller/simple/merge_test.go
@@ -193,3 +193,86 @@ func TestMerger_diff(t *testing.T) {
 		})
 	}
 }
+
+func TestMerger_reverseIndex(t *testing.T) {
+	type fields struct {
+		Info    *CommitInfo
+		Base    *Tree
+		Latest  *Tree
+		Current *Tree
+	}
+	type args struct {
+		tree *Tree
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   map[uint64][]uint64
+	}{
+		{
+			name: "empty_tree",
+			args: args{
+				tree: &Tree{
+					RootIno: 1,
+					Inodes:  map[uint64]*File{},
+				},
+			},
+			want: map[uint64][]uint64{},
+		}, {
+			name: "normal_tree",
+			args: args{
+				tree: &Tree{
+					RootIno: 1,
+					Inodes: map[uint64]*File{
+						1: { // /
+							FileType: FileType_Directory,
+							Entries: map[string]uint64{
+								"bin": 2,
+								"tmp": 5,
+							},
+						},
+						2: { // /bin/
+							FileType: FileType_Directory,
+							Entries: map[string]uint64{
+								"sh":   3,
+								"bash": 4,
+							},
+						},
+						3: { // /bin/sh
+							FileType: FileType_SymbolicLink,
+						},
+						4: { // /bin/bash
+							FileType: FileType_Regular,
+						},
+						5: { // /tmp/
+							FileType: FileType_Directory,
+							Entries: map[string]uint64{
+								"bash": 4,
+							},
+						},
+					},
+				},
+			},
+			want: map[uint64][]uint64{
+				2: {1},
+				3: {2},
+				4: {2, 5},
+				5: {1},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &Merger{
+				Info:    tt.fields.Info,
+				Base:    tt.fields.Base,
+				Latest:  tt.fields.Latest,
+				Current: tt.fields.Current,
+			}
+			if got := m.reverseIndex(tt.args.tree); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("reverseIndex() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
