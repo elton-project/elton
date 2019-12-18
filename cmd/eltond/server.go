524fe30378d2a97cd665004fa8b1f267031ab122
yuuki <yuuki0xff@gmail.com>
2019-12-18 15:28:31 +0900

    start simple controller

--- cmd/eltond/server.go
+++ cmd/eltond/server.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"gitlab.t-lab.cs.teu.ac.jp/yuuki/elton/subsystems"
+	"gitlab.t-lab.cs.teu.ac.jp/yuuki/elton/subsystems/controller/simple"
 	localStorage "gitlab.t-lab.cs.teu.ac.jp/yuuki/elton/subsystems/storage/local"
 )
 
 func NewServer(role string) subsystems.Server {
 	switch role {
 	case "controller":
-		panic("todo")
+		return simple.NewServer()
 	case "storage":
 		return localStorage.NewLocalStorageServer()
 	default:
