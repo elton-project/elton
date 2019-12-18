feed1bc328a19fdb9d1fb1639d969a9a97227d3c
yuuki <yuuki0xff@gmail.com>
2019-12-18 18:15:19 +0900

    set timeout

--- eltonfs_rpc/apiclient.go
+++ eltonfs_rpc/apiclient.go
@@ -1,18 +1,23 @@
 package eltonfs_rpc
 
 import (
+	"context"
 	elton_v2 "gitlab.t-lab.cs.teu.ac.jp/yuuki/elton/api/v2"
 	"gitlab.t-lab.cs.teu.ac.jp/yuuki/elton/subsystems"
 	"golang.org/x/xerrors"
 	"google.golang.org/grpc"
 	"strconv"
+	"time"
 )
+
+const DefaultAPITimeout = 3 * time.Second
 
 type apiClient struct{}
 
 func (apiClient) dial(port int) (*grpc.ClientConn, error) {
 	address := "localhost:" + strconv.Itoa(port)
-	return grpc.Dial(address, grpc.WithInsecure())
+	ctx, _ := context.WithTimeout(context.Background(), DefaultAPITimeout)
+	return grpc.DialContext(ctx, address, grpc.WithInsecure())
 }
 func (apiClient) CommitService() (elton_v2.CommitServiceClient, error) {
 	cc, err := apiClient{}.dial(subsystems.ControllerPort)
