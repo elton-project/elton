d2342006d40107fafc343139f3b87833f1236e10
yuuki <yuuki0xff@gmail.com>
2019-12-19 16:36:32 +0900

    fix compile error

--- api/v2/apiclient.go
+++ api/v2/apiclient.go
@@ -18,17 +18,17 @@ func (ApiClient) dial(port int) (*grpc.ClientConn, error) {
 	ctx, _ := context.WithTimeout(context.Background(), DefaultAPITimeout)
 	return grpc.DialContext(ctx, address, grpc.WithInsecure())
 }
-func (ApiClient) CommitService() (elton_v2.CommitServiceClient, error) {
+func (ApiClient) CommitService() (CommitServiceClient, error) {
 	cc, err := ApiClient{}.dial(subsystems.ControllerPort)
 	if err != nil {
 		return nil, xerrors.Errorf("dial: %w", err)
 	}
-	return elton_v2.NewCommitServiceClient(cc), nil
+	return NewCommitServiceClient(cc), nil
 }
-func (ApiClient) StorageService() (elton_v2.StorageServiceClient, error) {
+func (ApiClient) StorageService() (StorageServiceClient, error) {
 	cc, err := ApiClient{}.dial(subsystems.StoragePort)
 	if err != nil {
 		return nil, xerrors.Errorf("dial: %w", err)
 	}
-	return elton_v2.NewStorageServiceClient(cc), nil
+	return NewStorageServiceClient(cc), nil
 }
