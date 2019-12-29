a2aa77fb3bf667626c178ae726afe79e4a95c112
yuuki <yuuki0xff@gmail.com>
2019-12-30 00:16:06 +0900

    fix warnings

--- eltonfs/include/elton/commit.h
+++ eltonfs/include/elton/commit.h
@@ -1,6 +1,10 @@
 #ifndef _ELTON_COMMIT_H
 #define _ELTON_COMMIT_H
 
+#include <elton/elton.h>
+#include <elton/rpc/struct.h>
+
 int get_commit_id_by_config(struct eltonfs_config *config, char **cid);
+int get_commit_info(char *cid, struct commit_info **info);
 
 #endif // _ELTON_COMMIT_H
