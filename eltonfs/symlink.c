29d16495593900add22551471d54304511928169
yuuki <yuuki0xff@gmail.com>
2020-01-03 23:39:47 +0900

    rename

--- eltonfs/symlink.c
+++ eltonfs/symlink.c
@@ -1,13 +1,13 @@
 #include <elton/elton.h>
 #include <elton/xattr.h>
 
-const char *eltonfs_symlink_get_link(struct dentry *dentry, struct inode *inode,
-                                     struct delayed_call *done) {
+const char *eltonfs_get_link(struct dentry *dentry, struct inode *inode,
+                             struct delayed_call *done) {
   return eltonfs_i(inode)->symlink.redirect_to;
 }
 
 struct inode_operations eltonfs_symlink_inode_operations = {
-    .get_link = eltonfs_symlink_get_link,
+    .get_link = eltonfs_get_link,
 #ifdef ELTONFS_XATTRS
     .listxattr = elton_list_xattr_vfs,
 #endif
