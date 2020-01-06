4aa3b1e047f8b7ea4e41851ee02c777f9b80c75b
yuuki <yuuki0xff@gmail.com>
2020-01-06 15:52:54 +0900

    define eltonfs_ioctl_commit()

--- eltonfs/ioctl.c
+++ eltonfs/ioctl.c
@@ -1,12 +1,23 @@
 #include <elton/elton.h>
 #include <linux/uaccess.h>
 
+long eltonfs_ioctl_commit(struct super_block *sb) {
+  // todo: block write access.
+  // todo: build new tree from vfs_inode.
+  // todo: send tree.
+  // todo: wait response.
+  // todo: block all access.
+  // todo: change commit.
+  // todo: unblock all access.
+}
+
 long eltonfs_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
   struct inode *inode = file_inode(file);
+  struct super_block *sb = inode->i_sb;
 
   switch (cmd) {
   case ELTONFS_IOC_COMMIT:
-    // todo
+    return eltonfs_ioctl_commit(sb);
   case FS_IOC_GETFLAGS: {
     unsigned int flags = 0;
     // TODO: 拡張属性に対応する。
