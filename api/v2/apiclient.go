4bdcd167426354856b2e45c1136bf26c287a6e6d
yuuki <yuuki0xff@gmail.com>
2020-01-06 13:08:26 +0900

    load address from config file

--- api/v2/apiclient.go
+++ api/v2/apiclient.go
@@ -5,28 +5,55 @@ import (
 	"gitlab.t-lab.cs.teu.ac.jp/yuuki/elton/subsystems"
 	"golang.org/x/xerrors"
 	"google.golang.org/grpc"
+	"io/ioutil"
+	"log"
+	"os"
 	"strconv"
 	"time"
 )
 
+const ControllerConfigFile = "/etc/elton/controller-node"
+const StorageConfigFile = "/etc/elton/storage-node"
 const DefaultAPITimeout = 3 * time.Second
+
+var controllerURI string
+var storageURI string
+
+func init() {
+	controllerURI = readFileOrDefault(ControllerConfigFile, "localhost:"+strconv.Itoa(subsystems.ControllerPort))
+	storageURI = readFileOrDefault(StorageConfigFile, "localhost:"+strconv.Itoa(subsystems.StoragePort))
+}
+
+func readFileOrDefault(file string, defaultData string) string {
+	if _, err := os.Stat(file); err != nil {
+		if os.IsNotExist(err) {
+			log.Printf("%s: not found.  Using default settings")
+			return defaultData
+		}
+		log.Fatal(xerrors.Errorf("%s: %w", file, err))
+	}
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatal(xerrors.Errorf("%s: %w", file, err))
+	}
+	return string(data)
+}
 
 type ApiClient struct{}
 
-func (ApiClient) dial(port int) (*grpc.ClientConn, error) {
-	address := "localhost:" + strconv.Itoa(port)
+func (ApiClient) dial(address string) (*grpc.ClientConn, error) {
 	ctx, _ := context.WithTimeout(context.Background(), DefaultAPITimeout)
 	return grpc.DialContext(ctx, address, grpc.WithInsecure())
 }
 func (ApiClient) CommitService() (CommitServiceClient, error) {
-	cc, err := ApiClient{}.dial(subsystems.ControllerPort)
+	cc, err := ApiClient{}.dial(controllerURI)
 	if err != nil {
 		return nil, xerrors.Errorf("dial: %w", err)
 	}
 	return NewCommitServiceClient(cc), nil
 }
 func (ApiClient) StorageService() (StorageServiceClient, error) {
-	cc, err := ApiClient{}.dial(subsystems.StoragePort)
+	cc, err := ApiClient{}.dial(storageURI)
 	if err != nil {
 		return nil, xerrors.Errorf("dial: %w", err)
 	}
@@ -34,7 +61,7 @@ func (ApiClient) StorageService() (StorageServiceClient, error) {
 }
 
 func (ApiClient) VolumeService() (VolumeServiceClient, error) {
-	cc, err := ApiClient{}.dial(subsystems.ControllerPort)
+	cc, err := ApiClient{}.dial(controllerURI)
 	if err != nil {
 		return nil, xerrors.Errorf("dial: %w", err)
 	}
